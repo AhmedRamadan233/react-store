e185b7da71b9d09e8b660e85f035ab22c258dc50
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-09-30 00:05:34 +0300

    make class helper

--- src/Components/Home/HomeCategory.js
+++ src/Components/Home/HomeCategory.js
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import SubTiltle from "../Uitily/SubTiltle";
 import CategoryCard from "./../Category/CategoryCard";
-import clothe from "../../images/clothe.png";
-import cat2 from "../../images/cat2.png";
-import labtop from "../../images/labtop.png";
-import sale from "../../images/sale.png";
-import pic from "../../images/pic.png";
+// import clothe from "../../images/clothe.png";
+// import cat2 from "../../images/cat2.png";
+// import labtop from "../../images/labtop.png";
+// import sale from "../../images/sale.png";
+// import pic from "../../images/pic.png";
 import { useDispatch, useSelector } from "react-redux";
 import getAllCategory from "../../redux/actions/categoryAction";
 
@@ -19,19 +19,21 @@ const HomeCategory = () => {
   const category = useSelector((state) => state.allCategory.category);
   const loading = useSelector((state) => state.allCategory.loading);
   const colors = ["#FFD3E8", "#F4DBA5", "#55CFDF", "#FF6262", "#0034FF", "#FFD3E8"]
+  const imgURL = "http://localhost:8000/images/"
   return (
     <Container>
       <SubTiltle title="التصنيفات" btntitle="المزيد" pathText="/allcategory" />
       <Row className="my-2 d-flex justify-content-between">
         {category.data
           ? category.data
-              .slice(0,3)
+              .slice(0,4)
               .map((item , index) => (
                 <CategoryCard
                   key={item.id}
                   title={item.name}
-                  img={item.image}
+                  img={`${imgURL}${item.image}`}
                   background={colors[index]}
+                  
 
                 />
               ))
