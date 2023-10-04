8da3564b5445a4591e8cc136d1861527d7766be1
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-10-05 01:22:41 +0300

    make a  spinner

--- src/Components/Home/HomeCategory.js
+++ src/Components/Home/HomeCategory.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Spinner } from "react-bootstrap";
 import SubTiltle from "../Uitily/SubTiltle";
 import CategoryCard from "./../Category/CategoryCard";
 // import clothe from "../../images/clothe.png";
 // import cat2 from "../../images/cat2.png";
 // import labtop from "../../images/labtop.png";
 // import sale from "../../images/sale.png";
-// import pic from "../../images/pic.png";
+import loadingImg from "../../images/22.gif";
 import { useDispatch, useSelector } from "react-redux";
 import getAllCategory from "../../redux/actions/categoryAction";
-
+import "./HomeCategory.css";
 const HomeCategory = () => {
   const dispatch = useDispatch(); // Get the dispatch function from React-Redux
   useEffect(() => {
@@ -18,26 +18,43 @@ const HomeCategory = () => {
 
   const category = useSelector((state) => state.allCategory.category);
   const loading = useSelector((state) => state.allCategory.loading);
-  const colors = ["#FFD3E8", "#F4DBA5", "#55CFDF", "#FF6262", "#0034FF", "#FFD3E8"]
-  const imgURL = "http://localhost:8000/images/"
+  const colors = [
+    "#FFD3E8",
+    "#F4DBA5",
+    "#55CFDF",
+    "#FF6262",
+    "#0034FF",
+    "#FFD3E8",
+  ];
+  const imgURL = "http://localhost:8000/images/";
   return (
     <Container>
       <SubTiltle title="التصنيفات" btntitle="المزيد" pathText="/allcategory" />
       <Row className="my-2 d-flex justify-content-between">
-        {category.data
-          ? category.data
-              .slice(0,4)
-              .map((item , index) => (
+        {loading === false ? (
+          category.data ? (
+            category.data
+              .slice(0, 5)
+              .map((item, index) => (
                 <CategoryCard
                   key={item.id}
                   title={item.name}
                   img={`${imgURL}${item.image}`}
                   background={colors[index]}
-                  
-
                 />
               ))
-          : null}
+          ) : null
+        ) : (
+          <div className="spinner-container">
+            <Spinner
+              animation="border"
+              role="status"
+              className="custom-spinner"
+            >
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          </div>
+        )}
       </Row>
     </Container>
   );
